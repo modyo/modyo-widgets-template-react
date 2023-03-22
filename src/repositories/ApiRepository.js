46915ee782d9cf027343e6d0efc58c02bd422e24
Hassen <hnachar@gmail.com>
2023-03-22 12:35:25 +0100

    feat: vite start

--- src/repositories/ApiRepository.js
+++
@@ -1,9 +0,0 @@
-import ApiClient from './clients/ApiClient';
-
-const ApiRepository = {
-  summary() {
-    return ApiClient.get('summary');
-  },
-};
-
-export default ApiRepository
--- src/repositories/clients/ModyoClient.js
+++
@@ -1,8 +0,0 @@
-import { Client } from '@modyo/sdk';
-
-import liquidParser from '../../liquid/liquidParser';
-
-const accountUrl = liquidParser.parse('{{account.url}}');
-const LANG = liquidParser.parse('{{site.language}}');
-
-export default new Client(accountUrl, LANG);
