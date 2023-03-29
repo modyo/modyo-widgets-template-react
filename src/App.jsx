72fc6bc31a5c5cd94ff3662fc63d39de27afe948
Hassen <hnachar@gmail.com>
2023-03-29 18:06:22 +0200

    feat: eslint & prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
@@ -6,7 +6,7 @@ function App() {
       <h1>Modyo 💚</h1>
       <h2>React template</h2>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
