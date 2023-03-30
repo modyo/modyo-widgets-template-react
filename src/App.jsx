c582eb2ce480d956f461d02c57f3b59c1eeea398
Hassen <hnachar@gmail.com>
2023-03-30 16:44:20 +0200

    feat: basic modyo SDK

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
 import './App.css';
+import { Client } from '@modyo/sdk/dist/index';
+import { useEffect } from 'react';
 
 function App() {
+  const testSDK = async () => {
+    const client = new Client('https://fed-team.modyo.cloud', 'es');
+
+    const content = client.getContentType('animals', 'game');
+    const result = await content.getEntries();
+    return result;
+  };
+
+  useEffect(() => {
+    testSDK().then((result) => {
+      console.log('result: ', result);
+    });
+  });
+
   return (
     <div className="App">
       <h1>Modyo 💚</h1>
