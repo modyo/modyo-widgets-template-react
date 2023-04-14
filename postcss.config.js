e6696ee68bd8377d4ff94845375a9f70bba432b9
Hassen <hnachar@gmail.com>
2023-04-14 11:55:52 +0200

    feat: postcss dev mode

--- postcss.config.js
+++ postcss.config.js
@@ -2,11 +2,15 @@ import PurgeCss from '@fullhuman/postcss-purgecss';
 import Autoprefixer from 'autoprefixer';
 
 const purgeCssOptions = {
-  content: ['./src/**/*.html', './src/**/*.jsx'],
+  content: ['index.html', './src/**/*.jsx'],
 };
 
 const Config = {
-  plugins: [Autoprefixer, PurgeCss(purgeCssOptions)],
+  plugins: [Autoprefixer],
 };
+
+if (process.env.NODE_ENV === 'production') {
+  Config.plugins.push(PurgeCss(purgeCssOptions));
+}
 
 export default Config;
